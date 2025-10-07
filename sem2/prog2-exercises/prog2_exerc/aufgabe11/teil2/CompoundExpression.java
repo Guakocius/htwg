13fa0035f4463a5ab306dd38fee45147cc369804
Guakocius <engelhardt.alexander@mein.gmx>
2025-10-07 11:25:54 +0200

    Added all current directories

---
+++ sem2/prog2-exercises/prog2_exerc/aufgabe11/teil2/CompoundExpression.java
@@ -0,0 +1,14 @@
+package aufgabe11.teil2;
+
+import java.util.Map;
+import java.util.function.Function;
+
+public class CompoundExpression implements Expression {
+    public CompoundExpression() {
+    }
+
+    @Override
+    public double eval(Map<String, Double> varBel) {
+        return 1.0;
+    }
+}
