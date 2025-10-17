39a10aca5e23ed87dabf4e699039c1f300ef67c1
Guakocius <engelhardt.alexander@mein.gmx>
2025-10-17 11:17:32 +0200

    Added SE Project -- Civilization

---
+++ sem3/se/project/civ/src/main/scala/Map.scala
@@ -0,0 +1,42 @@
+package de.htwg.se
+
+import scalafx.scene.paint.Color
+
+val RESET = "\u001b[0m"
+val GREEN = "\u001b[32m"
+val YELLOW = "\u001b[33m"
+val WHITE = "\u001b[37m"
+val BLUE = "\u001b[34m"
+
+def isBoat(unit: PlayerUnit): Boolean = {
+  true
+}
+
+case class Tile(x: Int, y: Int, terrain: Terrain, var unit: Option[PlayerUnit] = None,
+  var city: Option[PlayerCity] = None) {
+    override def toString(): String = (if (terrain.color == Color.GREEN) GREEN else if (terrain.color == Color.BROWN) YELLOW else if (terrain.color == Color.BLUE) BLUE else WHITE) + terrain.symbol + RESET
+  }
+sealed trait Terrain { 
+  val moveCost: Int 
+  val color: Color
+  def symbol: String
+}
+case object Grassland extends Terrain { 
+  val moveCost = 1 
+  val color = Color.GREEN
+  val symbol = " . "
+}
+case object Mountain extends Terrain { 
+  val moveCost = 3
+  val color = Color.BROWN
+  val symbol = " ^ "
+}
+case object Ocean extends Terrain {
+  val moveCost = 2
+  val color = Color.BLUE
+  val symbol = " _ "
+}
+case class Player(name: String)
+case class PlayerUnit(name: String, owner: Player, var x: Int, var y: Int)
+case class PlayerCity(owner: Player, x: Int, y: Int)
+
