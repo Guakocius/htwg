0b6ca843838858f61376dcc450f1ab35bff588ec
Guakocius <engelhardt.alexander@mein.gmx>
2025-11-05 11:29:59 +0100

    Gitignore'd Mikrocontroller cache and TI stuff, added Stoch Homework

---
+++ sem3/stoch/homework/exerc3/solution_homework.py
@@ -0,0 +1,36 @@
+import numpy as np
+import statistics as stat
+from scipy import stats
+
+def main():
+    # Aufgabe 1.11
+    # Teilaufgabe 1.11.1
+    java_repos = np.array([9, 11, 9, 21, 16, 13, 0])
+    modal = stat.mode(java_repos)
+    mean = np.mean(java_repos)
+    median = np.median(java_repos)
+    ninety_quantile = np.quantile(java_repos, 0.9)
+    estd = np.std(java_repos, ddof=1)
+    q1 = np.quantile(java_repos, .25)
+    q2 = np.quantile(java_repos, .5)
+    q3 = np.quantile(java_repos, .75)
+    iqr = stats.iqr(java_repos)  # Interquartilabstand -- Delta(q1,q3) = q3 - q1
+    range = np.ptp(java_repos)  # max(java_repos) - min(java_repos) -- peak-to-peak
+
+    print(
+        "Modal: ", modal,
+        "\nMittel: ", mean,
+        "\nMedian: ", median,
+        "\n90%-Quantil: ", ninety_quantile,
+        "\nempirische Standardabweichung: ", estd,
+        f"\n25%-Quartil: {q1}\n50%-Quartil: {q2}\n75%-Quartil: {q3}",
+        "\nInterquartilabstand: ", iqr,
+        "\nSpannweite: ", range
+    )
+
+
+
+
+
+if __name__ == "__main__":
+    main()
