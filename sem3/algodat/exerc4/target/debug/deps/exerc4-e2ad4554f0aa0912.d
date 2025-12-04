858e7960ebf0fefa6aecee5fc7935cbdbc6eed2d
Guakocius <engelhardt.alexander@mein.gmx>
2025-12-04 09:19:00 +0100

    Added algodat and tgki

---
+++ sem3/algodat/exerc4/target/debug/deps/exerc4-e2ad4554f0aa0912.d
@@ -0,0 +1,5 @@
+/home/ciphercorsair/Programming/FH/htwg/sem3/algodat/exerc4/target/debug/deps/exerc4-e2ad4554f0aa0912.d: src/main.rs
+
+/home/ciphercorsair/Programming/FH/htwg/sem3/algodat/exerc4/target/debug/deps/exerc4-e2ad4554f0aa0912: src/main.rs
+
+src/main.rs:
