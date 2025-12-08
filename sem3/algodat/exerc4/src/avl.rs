7f30f2e0b39d720b43e77fb4ab055e877bbf92a9
Guakocius <engelhardt.alexander@mein.gmx>
2025-12-08 19:21:27 +0100

    Finished TGKI Versuch 2 and redone lab samples of Versuch 2 and Versuch 3

--- sem3/algodat/exerc4/src/avl.rs
+++ sem3/algodat/exerc4/src/avl.rs
@@ -20,7 +20,8 @@ pub trait AvlFn {
     fn in_order(&self);
     fn post_order(&self);
     fn search(&self, key: i32, value: i32, node: &NodePtr) -> bool;
-    fn remove(&self, key: i32, node: &NodePtr) -> bool;
+    fn remove(&mut self, key: i32, node: &mut NodePtr) -> bool;
+    fn search_min(&self, node: &NodePtr) -> NodePtr;
     fn traverse<F1, F2, F3>(&self, node: &NodePtr, pre: &F1, mid: &F2, post: &F3)
         where
             F1: Fn(i32),
@@ -87,7 +88,7 @@ impl AvlFn for AvlTree {
         self.traverse(&self.root, &|_| (), &|_| (), &|v| println!("visiting {}", v));
 
     }
-    fn search(&self, key: i32, mut value: i32, node: &NodePtr) -> bool {
+    fn search(&self, key: i32, value: i32, node: &NodePtr) -> bool {
         match node {
             None => {
                 return false;
@@ -105,30 +106,36 @@ impl AvlFn for AvlTree {
         //return if node.is_none() { false } else if 
         //key < node.key { self::search(key, value, node.left) } else if key > node.key { search(key, value, node.right) } else { value = node.value; return true; }
     }
-    fn remove(&self, key: i32, node: &mut NodePtr) -> bool {
-        let mut tmp: &mut NodePtr = node;
+
+    fn search_min(&self, p: &NodePtr) -> NodePtr {
+        if p.left.is_none() {
+            return p;
+        } else {
+            return search_min(&p.left);
+        }     }
+    fn remove(&mut self, key: i32, node: &mut NodePtr) -> bool {
         match node {
             None => {
-                return false;
-            } Some(_) => {
-                while let Some(p) = tmp {
+                return false
+            }
+            Some(ref mut p) => { 
                 if key < p.key {
-                    return self.remove(key, &p.left);    
+                    return self.remove(key, &mut p.left);
                 } else if key > p.key {
-                    return self.remove(key, &p.right);
+                    return self.remove(key, &mut p.right);
                 } else if p.left.is_none() || p.right.is_none() {
-                    if !p.left.is_none() {
-                        tmp = &mut p.left;
+                    if p.left.is_some() {
+                        p = &mut p.left; 
                     } else {
-                        tmp = &mut p.right;
+                        p = &mut p.right;
                     }
+                    return true; 
+                }
+                else {
+                    let min: searchMin(&p.right);
                     return false;
                 }
             }
-
-            } 
-            
-        }
-        return false;
+        }    
     }
 }
