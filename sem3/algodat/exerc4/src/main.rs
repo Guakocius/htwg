7b3310b599d9dbe77a3a197015c8fc63341f9607
Guakocius <engelhardt.alexander@mein.gmx>
2025-12-15 17:51:26 +0100

    Updated tgki versuch2 and Algodat

--- sem3/algodat/exerc4/src/main.rs
+++ sem3/algodat/exerc4/src/main.rs
@@ -1,8 +1,30 @@
 pub mod avl;
 
 use crate::avl::AvlTree;
-use crate::avl::AvlFn;
 
 fn main() {
-    let avl_tree = AvlTree::new();
+    let mut avl_tree = AvlTree::new();
+
+    // Einfügen von Knoten
+    avl_tree.insert(10, 100);
+    avl_tree.insert(20, 200);
+    avl_tree.insert(5, 50);
+    avl_tree.insert(15, 150);
+
+    // Suchen
+    println!("Suche 10: {}", avl_tree.search(10, 0));
+    println!("Suche 99: {}", avl_tree.search(99, 0));
+
+    // Traversierung
+    println!("\nIn-Order:");
+    avl_tree.in_order();
+
+    // Löschen
+    avl_tree.remove(20);
+    println!("\nNach Löschen von 20:");
+    avl_tree.in_order();
+    println!("\nPost Order:");
+    avl_tree.post_order();
+    println!("\nPre Order:");
+    avl_tree.pre_order();
 }
