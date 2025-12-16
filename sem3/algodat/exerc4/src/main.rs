ed1ad2ecb76b28e0d93d4a712c9ac6aa1fd6c6ce
Guakocius <engelhardt.alexander@mein.gmx>
2025-12-16 13:08:43 +0100

    Added algodat exerc5 directory

--- sem3/algodat/exerc4/src/main.rs
+++ sem3/algodat/exerc4/src/main.rs
@@ -12,19 +12,19 @@ fn main() {
     avl_tree.insert(15, 150);
 
     // Suchen
-    println!("Suche 10: {}", avl_tree.search(10, 0));
-    println!("Suche 99: {}", avl_tree.search(99, 0));
+    println!("Suche 10: {}", avl_tree.search(10, 0)); // true
+    println!("Suche 99: {}", avl_tree.search(99, 0)); // false
 
     // Traversierung
     println!("\nIn-Order:");
-    avl_tree.in_order();
-
-    // Löschen
-    avl_tree.remove(20);
-    println!("\nNach Löschen von 20:");
     avl_tree.in_order();
     println!("\nPost Order:");
     avl_tree.post_order();
     println!("\nPre Order:");
     avl_tree.pre_order();
+
+    // Löschen
+    avl_tree.remove(20);
+    println!("\nNach Löschen von 20:");
+    avl_tree.in_order();
 }
