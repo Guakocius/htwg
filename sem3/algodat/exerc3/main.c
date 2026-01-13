faed3dd0ff2500c92274804504caaada133d1224
Guakocius <engelhardt.alexander@mein.gmx>
2026-01-13 14:55:01 +0100

    Finished Algodat Exerc 5

--- sem3/algodat/exerc3/main.c
+++ sem3/algodat/exerc3/main.c
@@ -6,7 +6,8 @@ int main(void) {
     int n = 1000;
     int *arr = malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) {
-        int r = rand();
+        //int r = rand();
+        int r = i;
         *(arr + i) = r;
         printf("Array idx %d: %d\n", i, *(arr + i));
     }
@@ -20,16 +21,16 @@ int main(void) {
     //int cnti = i_sort(&b);
     int cntq = q_sort(&c, 0, n - 1);
     int cntm = m_sort(&aux, &d, 0, n - 1);
-    
+
    // printf("cnt selection sort: %d\n", cnts);
    // printf("cnt insertion sort: %d\n", cnti);
     printf("cnt quick sort: %d\n", cntq);
     printf("cnt merge sort: %d\n", cntm);
-    
 
-    
 
-    
 
-    
+
+
+
+
 }
