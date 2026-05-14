7cac5e7cdf80a0a8dd1e1465105c87f1f8b2106a
Guakocius <engelhardt.alexander@mein.gmx>
2026-05-14 12:39:16 +0200

    Added nonfunctional TCP client-to-server socket communication

--- sem5/compnet/labor/a5/group_chat/src/main.rs
+++ sem5/compnet/labor/a5/group_chat/src/main.rs
@@ -1,33 +1,28 @@
 mod client;
 mod server;
 
-use client::client::Client;
+use std::net::{TcpListener, TcpStream};
 
+use client::client::Client;
 use server::server::Server;
 
 fn main() {
     let mut server = Server::new();
     println!(
-        "Server with IP address {} on port {} initialized with client list {:?}",
+        "Server with IP address {} on port {} initialized with client list {:?}\n",
         server.ip, server.port, server.client_list
     );
 
     server.client_list.add_client();
+    let client = server.client_list.client_list.iter().last().unwrap();
+    server.port = client.clone().server_port;
 
     println!(
-        "New client registered: {} with IP address: {} and UDP port {}",
-        server
-            .client_list
-            .client_list
-            .iter()
-            .last()
-            .unwrap()
-            .username,
-        server.client_list.client_list.iter().last().unwrap().ip,
-        server.client_list.client_list.iter().last().unwrap().port
+        "\nNew client registered: {} with IP address: {} and UDP port {}\n",
+        client.username, client.ip, client.udp_port
     );
-    println!(
-        "Server with IP address {} on port {} initialized with client list {:?}",
-        server.ip, server.port, server.client_list
-    );
+
+    server.listen().unwrap();
+    client.connect_to_server(&server).unwrap();
+    //Server::receive(&mut stream);
 }
--- sem5/compnet/labor/a5/group_chat/src/server/server.rs
+++ sem5/compnet/labor/a5/group_chat/src/server/server.rs
@@ -1,12 +1,12 @@
-use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
 use std::thread;
-use std::time::Duration;
 
-use crate::client::client::{Client, ClientList};
+use crate::client::client::ClientList;
 
 pub struct Server {
     pub ip: String,
-    pub port: i32,
+    pub port: String,
     pub client_list: ClientList,
 }
 
@@ -14,12 +14,12 @@ impl Server {
     pub fn new() -> Server {
         Server {
             ip: String::from("127.0.0.1"),
-            port: 22,
+            port: String::from("5000"),
             client_list: ClientList::new(),
         }
     }
 
-    fn listen(&self) -> std::io::Result<()> {
+    pub fn listen(&self) -> std::io::Result<()> {
         let listener = TcpListener::bind(std::format!("{}:{}", self.ip, self.port))?;
 
         println!(
@@ -28,20 +28,36 @@ impl Server {
         );
 
         for stream in listener.incoming() {
+            println!("{:?}", stream);
             match stream {
-                Ok(stream) => {
-                    thread::spawn(|| Self::handle_client(stream));
+                Ok(mut stream) => {
+                    thread::spawn(move || Self::receive(&mut stream));
                 }
                 Err(e) => {
                     eprintln!("connection failed: {}", e);
                 }
             }
         }
-
+        match listener.accept() {
+            Ok((mut _socket, addr)) => {
+                println!("Incoming connection accepted: {:?}", addr);
+                thread::spawn(move || Self::receive(&mut _socket));
+            }
+            Err(e) => eprintln!("couldn't get client: {:?}", e),
+        }
         Ok(())
     }
-
-    fn handle_client(mut stream: TcpStream) {
-        println!("Incoming conneciton accepted: {:?}", stream.peer_addr());
+    pub fn receive(socket: &mut TcpStream) {
+        //let mut buf = [0; 68]; // NOTE: Change depending on size of client register packet
+        let mut buf = [0; 1024];
+        let data = socket.read(&mut buf[..]).expect("no data received");
+        if data == 0 {
+            println!("Connection closed from other side.\nClosing...");
+            socket
+                .shutdown(Shutdown::Both)
+                .expect("shutdown call failed");
+        } else {
+            println!("Received message: {} from {}", data, todo!())
+        }
     }
 }
