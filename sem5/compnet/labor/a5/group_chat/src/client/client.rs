189dad0132fbf22816a91ce9f989dea813f2bc56
Guakocius <engelhardt.alexander@mein.gmx>
2026-05-14 12:39:16 +0200

    Added nonfunctional TCP client-to-server socket communication

--- sem5/compnet/labor/a5/group_chat/src/client/client.rs
+++ sem5/compnet/labor/a5/group_chat/src/client/client.rs
@@ -1,7 +1,14 @@
-use std::io::{Result, Error, ErrorKind, stdin};
+use std::io::{Result, Error, ErrorKind, stdin, Write, Read};
+use std::net::TcpStream;
 use std::process;
 
 use regex::Regex;
+
+use crate::Server;
+
+const MIN_PORT_NUM: u32 = 1;
+const MAX_PORT_NUM: u32 = 65535;
+const LIN_EXIT_CODE: i32 = 0x0100;
 
 #[derive(Debug, PartialEq)]
 pub struct ClientList {
@@ -21,7 +28,8 @@ impl ClientList {
 pub struct Client {
     pub username: String,
     pub ip: String,
-    pub port: String,
+    pub server_port: String,
+    pub udp_port: String,
 }
 
 impl Client {
@@ -33,25 +41,24 @@ impl Client {
         let user_input = stdin();
         let mut username = String::new();
         let mut ip = String::new();
-        let mut port = String::new();
+        let mut udp_port = String::new();
 
         println!("Please register yourself. Type '|' to escape.");
 
         ["username", "IP address", "UDP port"]
             .into_iter()
-            .zip([&mut username, &mut ip, &mut port])
+            .zip([&mut username, &mut ip, &mut udp_port])
             .fuse()
             .for_each(|(k, v)| {
                 println!("Please enter your {}:", k);
                 user_input.read_line(v).expect("failed to readline");
                 if v.chars().any(|c| c == '|') {
                     println!("Closing register process");
-                    process::exit(0x0100);
+                    process::exit(LIN_EXIT_CODE);
                 }
                 *v = v.trim().to_string()
             });
 
-        println!("{}", port);
         if !Regex::new(r"[a-zA-Z0-9_-]{3,20}")
             .unwrap()
             .is_match(&username)
@@ -73,7 +80,7 @@ impl Client {
                 None,
                 Err(Error::new(ErrorKind::InvalidInput, "-2")) 
             )
-        } else if port.trim().parse::<u32>().unwrap() < 1 && port.trim().parse::<u32>().unwrap() > 65535 {
+        } else if udp_port.trim().parse::<u32>().unwrap() < MIN_PORT_NUM && udp_port.trim().parse::<u32>().unwrap() > MAX_PORT_NUM {
                 println!("UDP port number must follow this RegEx convention:
                     (6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{{2}}|6[0-4][0-9]{{3}}|[1-5][0-9]{{4}}|[0-9]{{1,4}})");
          
@@ -86,10 +93,38 @@ impl Client {
                 Some(Client {
                 username: username,
                 ip: ip,
-                port: port
+                server_port: String::from("5000"),
+                udp_port: udp_port
             }),
                 Ok(())
             )
+        }
+    }
+
+    fn send_udp(&self, port: String, stream: &mut TcpStream) -> Result<()> {
+        let mut pos = 0;
+        let port_bytes = port.as_bytes();
+        while pos <  port_bytes.len() {
+            let bytes_written = stream.write(&port_bytes[pos..]).unwrap();
+            pos += bytes_written;
+        }
+        Ok(())
+
+    }
+
+
+    pub fn connect_to_server(&self, server: &Server) -> Result<TcpStream> {
+        let ip = &server.ip;
+        let port = &server.port;
+        let mut msg = String::new();
+        println!("Connecting to the server with {} on port {}", ip, port);
+        
+        if let Ok(mut stream) = TcpStream::connect(std::format!("{}:{}", ip, port)) {
+            self.send_udp(self.udp_port.clone(), &mut stream).unwrap();
+            println!("Sending message {}", msg);
+            Ok(stream)
+        } else {
+            Err(Error::new(ErrorKind::ConnectionRefused, "Connection to the server has been refused"))
         }
     }
 }
