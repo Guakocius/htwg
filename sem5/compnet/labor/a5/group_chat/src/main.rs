c302a877f557231b3e478a052caf2779cb067fed
Guakocius <engelhardt.alexander@mein.gmx>
2026-05-19 16:51:23 +0200

    Rebuilt the code in Tokio async await

--- sem5/compnet/labor/a5/group_chat/src/main.rs
+++ sem5/compnet/labor/a5/group_chat/src/main.rs
@@ -4,7 +4,7 @@ mod server;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    if let Err(e) = run::run() {
+    if let Err(e) = run::run().await {
         eprintln!("error occured while trying to run: {e}");
     }
     Ok(())
